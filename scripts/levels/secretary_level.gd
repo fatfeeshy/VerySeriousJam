35f358af1b2d3e128f6424dc69dcc3ce2465db22
fatfishy <fatfishyy@gmail.com>
2026-06-21 11:36:41 -0500

    Finished secretary level

--- scripts/levels/secretary_level.gd
+++ scripts/levels/secretary_level.gd
@@ -24,37 +24,30 @@ func _process(_delta: float) -> void:
 
 func green_light():
 	randomize()
-	var more_green_time = randf_range(0.5, 2.5)
+	var more_green_time = randf_range(0.5, 2.0)
 	green_timer.wait_time = 3.0 + more_green_time
-	print("Green time:", green_timer.wait_time)
 	green_timer.start()
-	print("GREEN LIGHT START")
 
 func _on_green_timer_timeout() -> void:
 	transition_light()
-	transition_timer.start()
 
 func transition_light():
 	# Cool effect thing that is red
-	print("STOP!!!")
-	pass
+	transition_timer.start()
 
 func _on_transition_timer_timeout() -> void:
 	red_light()
 	red_timer.start()
 
 func red_light():
-	print("RED LIGHT")
 	red = true
-	if player.velocity != Vector2.ZERO:
+	if player.velocity != Vector2.ZERO or player_at_risk:
+		red_timer.stop()
+		red_timer.timeout.emit()
 		player.respawn()
-		red = false
-		green_timer.start()
-		green_light()
 
 func _on_red_timer_timeout() -> void:
 	red = false
-	green_timer.start()
 	green_light()
 
 # --- REDZONES --- #
