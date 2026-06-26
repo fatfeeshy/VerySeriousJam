a233ff413ad68d26e79076ea1bfb55d898b21ae9
micw1417 <wu355049@gmail.com>
2026-06-25 23:53:41 -0500

    Revert "Fixed commit merge issue hopefully"
    This reverts commit 01780a36f8c3d4a1b50b7a3c102753a2d54d2156, reversing
    changes made to 6ec32054966e6a6db63f6e70296fed571a8b71cd.

--- scripts/levels/blcoks.gd
+++ scripts/levels/blcoks.gd
@@ -1,10 +1,9 @@
-extends Node2D
-@onready var player: CharacterBody2D = $Player
+extends TileMapLayer
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	player.set_checkpoint_at(global_position)
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
---
+++ scripts/levels/buttonOb1.gd
@@ -0,0 +1,17 @@
+extends Area2D
+@onready var laser_ob_1: TileMapLayer = $"../Tiles/LaserOb1"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_area_entered(area: Area2D) -> void:
+	laser_ob_1.collision_enabled = false
+	laser_ob_1.modulate = Color(1, 1, 1, 0.2)
